49372b1d9548d5707f6b087c1edc8b8dfcb0af67
Jonathan Raine <98285281+RaiNeOnMe@users.noreply.github.com>
2022-12-26 11:51:36 +0700

    commentAdapter

---
+++ BeeKeeb/app/src/main/java/com/example/beekeeb/adapter/questionAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.beekeeb.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.beekeeb.R
+import com.example.beekeeb.model.Question
+
+class questionAdapter (private val questionList: ArrayList<Question>): RecyclerView.Adapter<questionAdapter.QuestionViewHolder>() {
+
+    var onItemClicked: ((Question)->Unit)? = null
+
+    class QuestionViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val profileImageView: ImageView = itemView.findViewById(R.id.profileIV)
+        val authorTextView: TextView = itemView.findViewById(R.id.authorTV)
+        val questionTextView: TextView = itemView.findViewById(R.id.questionTV)
+        val replyImageView: ImageView = itemView.findViewById(R.id.replyIV)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
+        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.comment_card, parent, false)
+        return QuestionViewHolder(inflater)
+    }
+
+    override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
+        val question = questionList[position]
+        holder.questionTextView.text = question.question
+        holder.authorTextView.text = question.authorName
+//        holder.profileImageView.image set profile image author queastion
+
+        holder.replyImageView.setOnClickListener{
+            onItemClicked?.invoke(question)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return questionList.size
+    }
+
+
+
+}
--- BeeKeeb/app/src/main/java/com/example/beekeeb/model/Question.kt
+++ BeeKeeb/app/src/main/java/com/example/beekeeb/model/Question.kt
@@ -2,5 +2,5 @@ package com.example.beekeeb.model
 
 import java.io.Serializable
 
-class Question (val question: String, val uid: String): Serializable {
+class Question (val question: String, val uid: String, val authorName: String, val authorPicture: String): Serializable {
 }
